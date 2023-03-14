d7156dd14787bf6ba4bba7538a18f6f6f32e438c
Akshay <jainakshay058@gmail.com>
2023-03-14 20:26:45 +0530

    Updates

---
+++ precache-manifest.eb27c2e2d498dfcfa6d6c2b5c6ba6a14.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9947a4b52a7e50816d2c631556c9d4",
+    "url": "/react-Ipod/index.html"
+  },
+  {
+    "revision": "5356a480bed08c88bb3b",
+    "url": "/react-Ipod/static/css/main.a2e7bd60.chunk.css"
+  },
+  {
+    "revision": "623d2a99ddffa95676e9",
+    "url": "/react-Ipod/static/js/2.4667406a.chunk.js"
+  },
+  {
+    "revision": "d8341c57f580e145d476bfdbdb6453b4",
+    "url": "/react-Ipod/static/js/2.4667406a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5356a480bed08c88bb3b",
+    "url": "/react-Ipod/static/js/main.62343ec5.chunk.js"
+  },
+  {
+    "revision": "0b0efe2dcdfafffbf105",
+    "url": "/react-Ipod/static/js/runtime-main.c40fb616.js"
+  },
+  {
+    "revision": "971e3181ae6735636e2c49cca0418f61",
+    "url": "/react-Ipod/static/media/John Denver - Country Roads.971e3181.jpg"
+  },
+  {
+    "revision": "a9f11dd7dc47db0768a140b89d89de83",
+    "url": "/react-Ipod/static/media/John Denver - Country Roads.a9f11dd7.mp3"
+  },
+  {
+    "revision": "2640f833069973cd0823c1f709b6353a",
+    "url": "/react-Ipod/static/media/Khalid - Young Dumb Broke.2640f833.jpg"
+  },
+  {
+    "revision": "e92d0309fbda5f2e2a063e0cc5834124",
+    "url": "/react-Ipod/static/media/Khalid - Young Dumb Broke.e92d0309.mp3"
+  },
+  {
+    "revision": "a085f2f0164d605a717084c32baf2274",
+    "url": "/react-Ipod/static/media/Never Gonna Give You Up.a085f2f0.png"
+  },
+  {
+    "revision": "4977e46cbdad85bad906db660d0a25a3",
+    "url": "/react-Ipod/static/media/Post Malone - White Iverson.4977e46c.mp3"
+  },
+  {
+    "revision": "68d6ebbb90f987df3665106e55131836",
+    "url": "/react-Ipod/static/media/Post Malone - White Iverson.68d6ebbb.png"
+  },
+  {
+    "revision": "ce3d38a985854c4640fe89ab293679ac",
+    "url": "/react-Ipod/static/media/Rick Astley - Never Gonna Give You Up.ce3d38a9.mp3"
+  },
+  {
+    "revision": "70ab2f27726c9ce5f3eeb1b54787a478",
+    "url": "/react-Ipod/static/media/Sigrid - High Five.70ab2f27.mp3"
+  },
+  {
+    "revision": "92a83443ea257110bbbedbaa7ac80d50",
+    "url": "/react-Ipod/static/media/Sigrid - High Five.92a83443.png"
+  },
+  {
+    "revision": "0f702e6d9615af4ea6e4503fa3754645",
+    "url": "/react-Ipod/static/media/game.0f702e6d.jpg"
+  },
+  {
+    "revision": "86f652825909378dd28f81eb87ed4dd3",
+    "url": "/react-Ipod/static/media/music.86f65282.jpg"
+  },
+  {
+    "revision": "626bd78a090cd747f7bfe1addd07babd",
+    "url": "/react-Ipod/static/media/settings.626bd78a.png"
+  },
+  {
+    "revision": "227f0ba9833ea10638eb851b4937d9f3",
+    "url": "/react-Ipod/static/media/wallpaper1.227f0ba9.jpg"
+  },
+  {
+    "revision": "23077a93618a97f5d7254432926d3d69",
+    "url": "/react-Ipod/static/media/wallpaper2.23077a93.jpg"
+  },
+  {
+    "revision": "b2fda5cab36fb65eccf326028ec5e1bb",
+    "url": "/react-Ipod/static/media/wallpaper3.b2fda5ca.jpg"
+  }
+]);
